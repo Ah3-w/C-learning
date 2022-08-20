7043cb1209090c564bb3b1cf3930fcceed7a559d
Ah3-w <98825253+Ah3-w@users.noreply.github.com>
2022-08-21 01:50:39 +0400

    Add files via upload

---
+++ first_project.cpp
@@ -0,0 +1,15 @@
+// My First C++ Project
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
+
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,17 @@
+//Learning Variables 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string charname = "Abdu";
+    int charage;
+    charage = 18;
+
+    cout << "guy named " << charname << ", " << charage << " yrs old"; 
+
+    return 0;
+
+}
